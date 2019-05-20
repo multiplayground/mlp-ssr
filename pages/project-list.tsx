8e0050e7ea1dc4daa91378e76556037a4dc726c8
MaxMitko <maxmitko@mail.ru>
2019-05-21 00:20:14 +0300

    feature(project-list): prefetch data

--- pages/project-list.tsx
+++ pages/project-list.tsx
@@ -1,10 +1,16 @@
 import Layout from '../layouts/main'
 import ProjectList from '../ui/ProjectList'
+import { getProjectList } from '../api/projectList'
 
 const ProjectListPage = () => (
     <Layout>
         <ProjectList />
     </Layout>
 )
+
+ProjectListPage.getInitialProps = async () => {
+    const data = await getProjectList({})
+    return data
+}
 
 export default ProjectListPage
