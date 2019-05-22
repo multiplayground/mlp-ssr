5ceee214a894ca867b296eb0656fb9621f30ec22
MaxMitko <maxmitko@mail.ru>
2019-05-22 04:02:51 +0300

    feature(Portal): new

---
+++ components/Portal/index.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+export type Props = {
+    selector: string
+    children?: React.ReactNode
+}
+
+const defaultProps = {
+    selector: 'modal-root',
+}
+
+const Portal = ({ selector, children }: Props) => {
+    const [element, setElement] = useState()
+
+    useEffect(() => {
+        if (!document) return
+        const wrapper = document.createElement('div')
+        wrapper.setAttribute('id', selector)
+        document.body.appendChild(wrapper)
+        const target = document.querySelector(`#${selector}`)
+        setElement(target)
+        return () => {
+            target && document.body.removeChild(target)
+        }
+    }, [selector])
+
+    return element ? ReactDOM.createPortal(children, element) : null
+}
+
+Portal.defaultProps = defaultProps
+
+export default Portal
