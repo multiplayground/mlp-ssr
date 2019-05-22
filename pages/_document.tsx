f3a0a9835eb95b4328264db9ad5a2fad85d39db5
MaxMitko <maxmitko@mail.ru>
2019-05-22 03:52:13 +0300

    refactor(_documents): make it more clear

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,37 @@
-import Document, { NextDocumentContext } from 'next/document'
+import Document, {
+    Head,
+    Main,
+    NextScript,
+    NextDocumentContext,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx: NextDocumentContext) {
+export default class MyDocument extends Document<{ styleTags: any }> {
+    static getInitialProps({ renderPage }: NextDocumentContext) {
+        // Step 1: Create an instance of ServerStyleSheet
         const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
 
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: App => props =>
-                        sheet.collectStyles(<App {...props} />),
-                })
+        // Step 2: Retrieve styles from components in the page
+        const page = renderPage((App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />)
+        )
 
-            const initialProps = await Document.getInitialProps(ctx)
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            }
-        } finally {
-            sheet.seal()
-        }
+        // Step 3: Extract the styles as <style> tags
+        const styleTags = sheet.getStyleElement()
+
+        // Step 4: Pass styleTags as a prop
+        return { ...page, styleTags }
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>{this.props.styleTags}</Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        )
     }
 }
