2e5db8ae64063b72d7e1dc9b16f1c416fd90f0d7
MaxMitko <maxmitko@mail.ru>
2019-05-22 04:00:31 +0300

    refactor(Button): cleaned

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ButtonStyled } from './styled'
-import styled, { PaletteBase, PaletteExtra } from 'styled-components'
+import { PaletteBase, PaletteExtra } from 'styled-components'
 
 export type Props = {
     variant: 'text' | 'outlined' | 'contained'
@@ -13,9 +13,8 @@ export type Props = {
     visibility?: boolean
     spinned?: boolean
     icon?: any | null
-    children?: React.ReactNode
     roundCorner?: boolean
-}
+} & React.ComponentPropsWithRef<'button'>
 
 const defaultProps = {
     variant: 'contained',
@@ -25,20 +24,9 @@ const defaultProps = {
 }
 
 const Button = (props: Props) => {
-    return (
-        <ButtonStyled {...props}>
-            <ChildrenWrapper>{props.children}</ChildrenWrapper>
-        </ButtonStyled>
-    )
+    return <ButtonStyled {...props}>{props.children}</ButtonStyled>
 }
 
 Button.defaultProps = defaultProps
 
 export default Button
-
-const ChildrenWrapper = styled.div`
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-items: center;
-`
